d72663742c6f9041c0899bba82e74fd95599b7f4
Mike Sowers <msowers72@yahoo.com>
2023-09-21 15:02:07 -0400

    declared js global variables

---
+++ index.js
@@ -0,0 +1,6 @@
+// get the geo-coordinates of entered city because it is requiredto ge weather details step 1
+const searchButton = document.querySelector(".search-btn");
+const cityInput = document.querySelector(".city-input");
+const locationButton = document.querySelector(".location-btn")
+const weatherCardsDiv = document.querySelector(".weather-cards")
+const currentWeatherDiv = document.querySelector(".current-weather")
